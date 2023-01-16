42cab6ebda13e2f3b2aa8f019efae179a6b9541d
Csaba79-coder <csabavadasz79@gmail.com>
2023-01-16 09:50:09 +0100

    INIT

---
+++ src/test/java/com/csaba79coder/bookandauthor/BookAndAuthorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.bookandauthor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAndAuthorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
